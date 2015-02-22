b4f15d948d447e53c2a6afd69d64b1ea062fb647
Pine Mizune <pinemz@gmail.com>
2015-02-22 10:57:07 +0900

    Add descriptions

--- .ci/deploy.bash
+++ .ci/deploy.bash
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+echo "> lein ring uberjar"
 lein ring uberjar
+
 git clone git@bitbucket.org:pine613/yurie-private.git
--- .ci/install.bash
+++ .ci/install.bash
@@ -4,5 +4,8 @@ curl -O https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein
 chmod +x lein
 sudo mv lein /usr/local/bin
 
+echo "> lein version"
 lein version
+
+echo "> lein deps"
 lein deps
